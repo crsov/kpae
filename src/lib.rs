4f8260b1aac7a3af65bc354b356f0a3703809ff5
crsov <aruthecrsov@yandex.ru>
2022-12-29 15:02:38 +0300

    Initial API implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::process::Stdio;
+
+use bytes::BytesMut;
+use futures_core::Stream;
+use futures_sink::Sink;
+use futures_util::StreamExt;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::process::Command;
+
+use serde::{Deserialize, Serialize};
+use tokio_stream::wrappers::LinesStream;
+use tokio_util::codec::{Encoder, FramedWrite};
+
+#[derive(Deserialize)]
+pub struct KataResponse;
+
+#[derive(Serialize)]
+pub struct KataQuery;
+
+pub async fn start(mut cmd: Command) -> (impl Sink<KataQuery>, impl Stream<Item = KataResponse>) {
+    let mut handle = cmd
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let stdin = handle.stdin.unwrap();
+    let stdout = BufReader::new(handle.stdout.take().unwrap());
+
+    (
+        FramedWrite::new(stdin, KataQueryEncoder),
+        LinesStream::new(stdout.lines())
+            .map(|x| x.unwrap())
+            .map(|line| serde_json::from_str::<KataResponse>(&line).unwrap()),
+    )
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct KataQueryEncoder;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Encoder<KataQuery> for KataQueryEncoder {
+    type Error = std::io::Error;
+
+    fn encode(&mut self, item: KataQuery, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        dst.extend_from_slice(serde_json::to_vec(&item).unwrap().as_slice());
+        Ok(())
     }
 }
